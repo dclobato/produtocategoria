1f5eac2d6061e61465d9ad1882a33b28366c0ddc
Daniel Correa Lobato <daniel.lobato@ifsp.edu.br>
2024-03-06 20:44:09 -0300

    Início do menu da aplicação
    0. Seed do banco de dados
    1. Incluir nova categoria
    2. Listar as categorias

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class Categoria(Base, DatasMixin):
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
     nome = Column(String(256), nullable=False)
 
+    # Propriedade de navegação
     lista_de_produtos = relationship("Produto", back_populates="categoria",
                                      cascade="all, delete-orphan", lazy="selectin")
 
@@ -46,26 +47,68 @@ class Produto(Base, DatasMixin):
     ativo = Column(Boolean, default=True)
     categoria_id = Column(Uuid(as_uuid=True), ForeignKey("tbl_categorias.id"))
 
+    # Propriedade de navegação
     categoria = relationship("Categoria", back_populates="lista_de_produtos")
 
 
-# cat = Categoria()
-# cat.nome = "Salgadinhos"
-#
-# prod = Produto()
-# prod.nome = "Doritos, 600g"
-# prod.ativo = True
-# prod.preco = 12.74
-# prod.estoque = 0
-# prod.categoria = cat
-#
-# with Session(motor) as sessao:
-#     sessao.add(prod)
-#     sessao.commit()
+def seed_database():
+    with Session(motor) as sessao:
+        if sessao.execute(select(Categoria).limit(1)).scalar_one_or_none():
+            return
+        from seed import seed_data
+        for categoria in seed_data:
+            cat = Categoria()
+            print(f"Semeando a categoria {categoria['categoria']}...")
+            cat.nome = categoria["categoria"]
+            for produto in categoria["produtos"]:
+                p = Produto()
+                p.nome = produto["nome"]
+                p.preco = produto["preco"]
+                p.estoque = 0
+                p.ativo = True
+                p.categoria = cat
+                sessao.add(p)
+            sessao.commit()
 
-with Session(motor) as sessao:
-    lista_de_categorias = sessao.execute(select(Categoria)).scalars()
-    for categoria in lista_de_categorias:
-        print(f"A categoria {categoria.nome} tem {len(categoria.lista_de_produtos)} produtos")
-        for produto in categoria.lista_de_produtos:
-            print(f"    Produto {produto.nome} que tem {produto.estoque} unidades no estoque")
+def incluir_categoria():
+    print("Incluindo categoria")
+    nome = input("Qual o nome da categoria que você quer adicionar? ")
+    with Session(motor) as sessao:
+        categoria = Categoria()
+        categoria.nome = nome
+        sessao.add(categoria)
+        sessao.commit()
+    print(f"Categoria {nome} adicionada.")
+
+
+def listar_categorias():
+    print("Categorias cadastradas")
+    print(f"Nome                                      # Produtos")
+    print(f"----------------------------------------- ----------")
+    stmt = select(Categoria)
+    stmt = stmt.order_by("nome")
+    with Session(motor) as sessao:
+        rset = sessao.execute(stmt).scalars()
+        for categoria in rset:
+            print(f"{categoria.nome:40s}  {len(categoria.lista_de_produtos):10d}")
+            # for produto in categoria.lista_de_produtos:
+            #     print(f"   {produto.nome}")
+    print(f"----------------------------------------- ----------")
+
+
+if __name__ == "__main__":
+    seed_database()
+    while True:
+        print("Menu de opcoes")
+        print("1. Incluir categoria")
+        print("2. Listar categorias")
+        print("0. Sair")
+        opcao = int(input("Qual opcao? "))
+        if opcao == 1:
+            incluir_categoria()
+        elif opcao == 2:
+            listar_categorias()
+        elif opcao == 0:
+            exit(0)
+        else:
+            print("Opcao invalida...")
